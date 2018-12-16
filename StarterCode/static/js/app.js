8ddd93aea29114cd39342521e2da00b78c7b261d
Nicholas Radich <nicholasradich1@gmail.com>
2018-12-16 12:40:18 -0800

    Was able to input the data into the table

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,9 +3,37 @@ var tableData = data;
 
 
 //selecting the ufo-table
-var ufoTable = d3.select('#ufo-table');
+const ufoTable = d3.select('#ufo-table');
 
-console.log(ufoTable.html());
+const columns = ['datetime','city', 'state', 'country', 'shape', 'durationMinutes', 'comments' ];
+
+const tbody = ufoTable.append('tbody');
+
+
+// create a row for each object in the table
+var rows = tbody.selectAll('tr')
+        .data(tableData)
+        .enter()
+        .append('tr');
+
+// create a cell for each row  
+
+const cells = rows.selectAll('td')
+            .data( function(row){
+                return columns.map(function(column){
+                    return {column: column, value: row[column]}
+                })
+            })
+            //input the data
+            .enter()
+            .append('td')
+            .text(function(d,i){
+                return d.value;
+            })
+            
+
+
+
 
 // now have to input data with a for each function
 
