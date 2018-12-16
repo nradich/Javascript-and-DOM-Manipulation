553dab37164a240d095f858226764c3bfa208061
Nicholas Radich <nicholasradich1@gmail.com>
2018-12-16 11:36:32 -0800

    Uploading initial documents

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,15 @@
+// from data.js
+var tableData = data;
+
+
+//selecting the ufo-table
+var ufoTable = d3.select('#ufo-table');
+
+console.log(ufoTable.html());
+
+// now have to input data with a for each function
+
+//then work on button/search functionality 
+
+
+// YOUR CODE HERE!
